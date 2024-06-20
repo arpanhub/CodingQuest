3cec4047ee53ffc1637fda61e3675dfd491a8a0c
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-06-20 08:19:19 +0530

    Time: 126 ms (57.07%) | Memory: 109.1 MB (6.13%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool Minimum_Magnetic_force(vector<int> position,int mid ,int m){
+        int balls = 1;
+        int pos = position[0];
+        for(int i = 0;i<position.size();i++){
+            if(position[i] - pos >= mid){
+                balls++;
+                pos = position[i];
+            }
+            if(balls == m){
+                return true;
+            }
+        }
+        return false;
+    }
+    int maxDistance(vector<int>& position, int m) {
+        int n = position.size();
+        sort(position.begin(),position.end());
+        int low = 0;
+        int high = position[n-1];
+        int MMF=0;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(Minimum_Magnetic_force(position,mid,m)){
+                MMF = mid;
+                low = mid  + 1;
+            }else{
+                high = mid -1 ;
+            }
+        }
+        return MMF;
+    }
+};

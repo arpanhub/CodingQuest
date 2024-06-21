3d89d536746fb51d93bc7c53486ba258ecc9ffa1
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-06-21 15:24:46 +0530

    Time: 43 ms (99.69%) | Memory: 44.9 MB (72.46%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        
-        unordered_map<int, int> m;
-        int count=0;
-        int sum=0;
-        
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++){
-            
-            sum=sum+nums[i];
-            
-            if(sum == k) 
-                count++;
-            
-            if(m.find(sum-k)!=m.end()){ //O(1)
-                count=m[sum-k]+count; 
+        int sum = 0;
+        unordered_map <int,int> mp;
+        // mp[0] = -1;
+        int count = 0;
+        for(int i = 0;i < nums.size();i++)
+        {
+            sum += nums[i];
+            if(sum == k) count++;//1,
+            if(mp.find(sum-k) != mp.end()){
+                count = mp[sum-k] + count;
             }
-            
-            if(m.find(sum)!=m.end()){
-                m[sum]++;
-                
-            }else{
-                m[sum]=1;
+            if(mp.find(sum) != mp.end()){
+                mp[sum]++;
             }
-            
+            else{
+                mp[sum] = 1;
+            }
         }
-        
         return count;
     }
 };
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();

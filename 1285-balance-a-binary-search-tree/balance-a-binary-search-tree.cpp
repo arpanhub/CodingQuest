59f28051b3a87de69208dc7a3c4ec7870af82219
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-06-28 11:09:12 +0530

    Time: 55 ms (98.84%) | Memory: 62.8 MB (20.61%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -1,3 +1,9 @@
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -33,12 +39,6 @@ public:
         return BuildBST(low,high,v);
     }
 };
-const int ZERO = []()
-{
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	return 0;
-}();
 
 
 

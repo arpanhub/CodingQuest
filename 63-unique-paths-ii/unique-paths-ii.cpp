428585e2dbf87fc9e107a31e0f6227948f1a017a
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-07-05 15:29:16 +0530

    Time: 78 ms (20.66%) | Memory: 72 MB (8.49%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int CountMoves(int idx_i,int idx_j,int row,int column,vector<vector<int>> obstacleGrid,vector<vector<int>>& dp){
+        if(idx_i == row-1 && idx_j == column -1 && obstacleGrid[idx_i][idx_j] != 1){
+            return 1;
+        }
+        if(idx_i == row || idx_j == column || obstacleGrid[idx_i][idx_j] == 1) return 0;
+        if(dp[idx_i][idx_j] != -1) return dp[idx_i][idx_j];
+        dp[idx_i][idx_j] =  CountMoves(idx_i+1,idx_j,row,column,obstacleGrid,dp) + CountMoves(idx_i,idx_j+1,row,column,obstacleGrid,dp);
+        return dp[idx_i][idx_j];
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+       int cnt = 0;
+        vector<vector<int>> dp(obstacleGrid.size() + 1, vector<int>(obstacleGrid[0].size() + 1, -1));
+       return CountMoves(0,0,obstacleGrid.size(),obstacleGrid[0].size(),obstacleGrid,dp);
+    }
+};

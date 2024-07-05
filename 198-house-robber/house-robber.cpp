57a4a5cd3c5d2cb7c290c62904e52959ca0b1b6f
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-07-05 15:57:15 +0530

    Time: 4 ms (31.34%) | Memory: 9.2 MB (35.01%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int calc(int index,vector<int>& nums,vector<int>& dp){
+        if(index >= nums.size())
+             return 0;
+        if(dp[index] != -1) return dp[index];
+        // int m1 = nums[index] + calc(index + 2,nums);
+        // int m2 = calc(index + 1,nums);
+        dp[index] = max(nums[index] + calc(index + 2,nums,dp),calc(index + 1,nums,dp));
+        return dp[index];
+    }
+    int rob(vector<int>& nums) {
+        int index = 0;
+        vector<int> dp(nums.size() +1,-1);
+        return calc(0,nums,dp);
+    }
+};

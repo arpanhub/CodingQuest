bebd4afb6bec621fa11e36366e89265538993e3c
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-07-06 11:38:33 +0530

    Time: 51 ms (14.53%) | Memory: 81.8 MB (8.01%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDist(string s1,string s2,int i,int j,vector<vector<int>>& dp){
+        if(i == s1.length()){
+            return s2.length()-j;
+        }
+        if(j == s2.length()){
+            return s1.length()-i;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s1[i] == s2[j]) return dp[i][j] = minDist(s1,s2,i + 1,j + 1,dp);
+        return dp[i][j] = min({(1+minDist(s1,s2,i + 1,j + 1,dp)),(1+minDist(s1,s2,i + 1,j,dp)),(1 +minDist(s1,s2,i,j + 1,dp))});
+    }
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,-1));
+       return  minDist(word1,word2,0,0,dp);
+    }
+};

1faf5309a11b50873f0a96bf2bf17855ce1e2614
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-07-10 12:18:27 +0530

    Time: 150 ms (59.48%) | Memory: 76.7 MB (37.03%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        int n = customers.size();
+        int cust_arrival_time = customers[0][0];//2
+        int cooking_finished_time = customers[0][0] + customers[0][1];//5
+        double cust_waiting_time = cooking_finished_time - cust_arrival_time;//3
+        for(int i =1;i < customers.size();i++){
+            if(customers[i][0] > cooking_finished_time){
+                cust_waiting_time +=customers[i][1];//6
+                cooking_finished_time = customers[i][0] + customers[i][1];
+            }else{
+                cooking_finished_time = cooking_finished_time + customers[i][1];//
+                cust_waiting_time += cooking_finished_time - customers[i][0];//
+            }
+        }
+        return cust_waiting_time/n;
+
+    }
+};

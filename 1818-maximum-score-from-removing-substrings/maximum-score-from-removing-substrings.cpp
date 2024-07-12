1c4fcdbdae9262dfd993b2b69928931da2a1b0cc
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-07-12 12:19:04 +0530

    Time: 181 ms (9.35%) | Memory: 37.3 MB (5.28%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxAmount(string& s,string find_str,int gains){
+        int n = s.length();
+        stack<int> st;
+        int points = 0;
+        for(int i = 0;i < n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+            } 
+            else if(st.top() == find_str[0] && s[i] == find_str[1]){
+                points += gains;
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+        s="";
+        while(!st.empty()){
+            s += st.top();
+            st.pop();
+        }
+        reverse(s.begin(),s.end());
+        return points;
+    }
+    int maximumGain(string s, int x, int y) {
+        string s1 = "ab";
+        string s2 = "ba";
+        if(y>x){
+            swap(s1,s2);
+            swap(x,y);
+        }
+        int amount1 =  maxAmount(s,s1,x);
+        int amount2 =  maxAmount(s,s2,y);
+        return amount1 + amount2;
+    }
+};

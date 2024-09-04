7690d7b2df7b97cdfaa85392251cbe1725872be7
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-09-04 15:28:02 +0530

    Time: 4 ms (33.22%) | Memory: 8 MB (40.78%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.cpp
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.cpp
@@ -3,6 +3,6 @@ public:
     bool checkTwoChessboards(string coordinate1, string coordinate2) {
         int a = coordinate1[0]-'0' + coordinate1[1];
         int b = coordinate2[0]-'0'+ coordinate2[1];
-        return (a%2 == b%2);
+        return !((a+b)%2);
     }
 };

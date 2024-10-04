cda38b36b5f4ff191a010b8d327b34dad5bc31b8
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-10-04 13:41:43 +0530

    Time: 146 ms (31.61%) | Memory: 70.5 MB (73.83%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,36 +1,25 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        long totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+        int sum = 0;
+        for(int i:nums){
+            sum +=i;
+            sum = sum %p;
         }
-
-        // Find the remainder when total sum is divided by p
-        int rem = totalSum % p;
-        if (rem == 0) return 0; // If the remainder is 0, no subarray needs to be removed
-
-        unordered_map<int, int> prefixMod;
-        prefixMod[0] = -1;  // Initialize for handling full prefix
-        long prefixSum = 0;
-        int minLength = nums.size();
-
-        for (int i = 0; i < nums.size(); ++i) {
-            prefixSum += nums[i];
-            int currentMod = prefixSum % p;
-            int targetMod = (currentMod - rem + p) % p;
-
-            if (prefixMod.find(targetMod) != prefixMod.end()) {
-                minLength = min(minLength, i - prefixMod[targetMod]);
+        if(sum%p == 0) return 0;
+        int target = sum%p;
+        int cumulative_sum = 0;
+        int length=nums.size();
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        for(int i = 0;i < nums.size();i++){
+            cumulative_sum =(cumulative_sum + nums[i])%p;  
+            int prev = (cumulative_sum-target + p)%p;
+            if(mp.find(prev)!=mp.end()){
+                length = min(length,i-mp[prev]);
             }
-
-            prefixMod[currentMod] = i;
+            mp[cumulative_sum] = i;
         }
-
-        return minLength == nums.size() ? -1 : minLength;
+        return length==nums.size()?-1:length;
     }
 };

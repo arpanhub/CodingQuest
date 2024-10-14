297b1d9367fb14e3759efd603760f4e2ceda1b95
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2024-10-14 11:38:29 +0530

    Time: 193 ms (63.59%) | Memory: 79.8 MB (50.62%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int i:nums){
+            pq.push(i);
+        }
+        // cout<<pq.top();
+        long long sum = 0;
+        double num;
+        long long num1;
+        while(k--){
+            num = pq.top();
+            pq.pop();
+            sum += num;
+            num = (num /3.0);
+            // cout<<num<<endl;
+            num1 = ceil(num);
+            // cout<<num1<<" ";
+            pq.push(num1);
+            // cout<<pq.top()<<" ";
+        }
+        return sum;
+    }
+};

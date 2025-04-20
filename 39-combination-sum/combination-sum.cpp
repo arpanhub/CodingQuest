009129dba2b45f3b4ad94f6ecd21f4548015ee5d
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2025-04-20 13:11:18 +0530

    Time: 4 ms (43.82%) | Memory: 14.4 MB (43.89%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& candidates,int target,int start,vector<int>& path){
+        if(target == 0){
+            result.push_back(path);
+        }
+        if(target < 0){
+            return;
+        }
+        for(int i = start; i < candidates.size();i++){
+            if( i > start && candidates[i] == candidates[i-1]) continue;
+            path.push_back(candidates[i]);
+            backtracking(candidates,target-candidates[i],i,path);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        //sort(begin(candidates),end(candidates));
+        backtracking(candidates,target,0,path);
+        return result;
+    }
+};

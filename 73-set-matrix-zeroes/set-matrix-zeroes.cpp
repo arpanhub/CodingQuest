e00cd4142de4ca76a6716d3bd0b676e2484a1611
Arpan Gupta <120000843+arpanhub@users.noreply.github.com>
2025-05-19 01:42:57 +0530

    Time: 2 ms (13.70%) | Memory: 18.8 MB (24.55%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,23 +3,25 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> rows(n,0);
-        vector<int> col(m,0);
+        vector<int> row(n,-1);
+        vector<int> col(m,-1);
         for(int i = 0;i < matrix.size();i++){
-            for(int j = 0;j<matrix[0].size();j++){
-                if(matrix[i][j] == 0){
-                    rows[i]++;
-                    col[j]++;
+            for(int j = 0;j< matrix[0].size();j++){
+                if(matrix[i][j]==0){
+                    row[i] = 0;
+                    col[j] = 0;
                 }
             }
         }
+        for(int i:row)cout<<i<<" ";
+        cout<<endl;
+        for(int k:col)cout<<k<<" ";
         for(int i = 0;i < matrix.size();i++){
-            for(int j = 0;j<matrix[0].size();j++){
-                if(rows[i] >0 || col[j] > 0){
-                    matrix[i][j] = 0;
+            for(int j = 0;j< matrix[0].size();j++){
+                if(row[i]==0 || col[j]==0){
+                    matrix[i][j]=0;
                 }
             }
         }
-
     }
 };
